005201b6eeff898c0ea09ab37615f423344a1f86
João Pedro Alves <jotalmeida007@hotmail.com>
2023-10-16 01:53:45 -0300

    chore: remove dist

--- frontend/js/contact-form.js
+++ frontend/js/contact-form.js
@@ -1,24 +1,22 @@
-import axios from "axios"
-
 document
   .getElementById("contact-form")
   .addEventListener("submit", function (event) {
+    listMessages()
+
     event.preventDefault()
 
     var name = document.getElementById("form-name").value
     var email = document.getElementById("form-email").value
     var message = document.getElementById("form-message").value
-
-    listMessages()
   })
 
 function createMessage(name, email, message) {}
 
 async function listMessages() {
   await axios
-    .get("localhost:3000/list-messages")
+    .get("0.0.0.0:3000/list-messages")
     .then((response) => {
-      console.log(response)
+      console.log(response.data)
     })
     .catch((error) => {
       console.log(error)
