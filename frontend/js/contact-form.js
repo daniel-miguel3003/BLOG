604dbd17f6301525dde812ad10f7f9c6878a9532
João Pedro Alves <jotalmeida007@hotmail.com>
2023-10-16 01:41:36 -0300

    chore: remove bugs from html

--- frontend/js/contact-form.js
+++ frontend/js/contact-form.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 document
   .getElementById("contact-form")
   .addEventListener("submit", function (event) {
@@ -6,4 +8,19 @@ document
     var name = document.getElementById("form-name").value
     var email = document.getElementById("form-email").value
     var message = document.getElementById("form-message").value
+
+    listMessages()
   })
+
+function createMessage(name, email, message) {}
+
+async function listMessages() {
+  await axios
+    .get("localhost:3000/list-messages")
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
